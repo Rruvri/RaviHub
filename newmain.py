9570cd8658f66a2a20a43406c7c184d59c4b4523
Rruvri <ravinathwani@btopenworld.com>
2025-08-28 14:54:20 +0100

    menu functions work as intended, moving on to pickle save data

--- newmain.py
+++ newmain.py
@@ -1,20 +1,51 @@
+import os
+import sys
+import time
 from newobjects import *
+
+def clear_console():
+    os.system('clear')
+    
 
 collections = []
 
 
-           
+    
+
+
+
 def main():
     while True:
         print("======= RaviHub =======")
         print("Open Collections:")
         for col in collections:
             print(f"{col.name}: {[item.name for item in col.items]}")
-        print("=====================")
+        print("=======================")
 
-        menu_choice = input("Actions:\n[1]Create new item\n[2]Create new collection\n[3]View collection")
+        menu_choice = input("Actions:\n[1]Create new item\n[2]Create new collection\n[3]View a collection\nEnter choice: ")
         if menu_choice == "1":
+            clear_console()
             create_item(collections)
+        elif menu_choice == "2":
+            clear_console()
+            create_collection(collections)
+        elif menu_choice == "3":
+            clear_console()
+            if not collections:
+                    print("No collections yet, returning to main menu...")
+                    time.sleep(2)
+                    clear_console()
+                    
+            for col in collections:
+                print(f"[{collections.index(col) + 1}] {col.name}\n") 
+            view_choice = input("Enter collection [number]: ")
+            clear_console()
+            collections[int(view_choice) -1].view_collection()
+
+            
+         
+            
+
          
 
         
--- newobjects.py
+++ newobjects.py
@@ -1,3 +1,7 @@
+import time
+import os
+def clear_console():
+    os.system('clear')
 
 class Item:
     def __init__(self, name, subcat):
@@ -8,6 +12,16 @@ class Collection:
     def __init__(self, name):
         self.name = name
         self.items = []
+    def view_collection(self):
+        clear_console()
+        print(f"======== {self.name} ========")
+        if not self.items:
+            print("(empty!)")
+            time.sleep(1.5)
+            clear_console()
+        for item in self.items:
+            print(f"{item.name}[{item.subcat}]\n")
+        
         
         
 def create_item(collections):        
@@ -20,10 +34,23 @@ def create_item(collections):
     for c in collections:
         if col.lower() == c.name.lower():
             c.items.append(item)
+            clear_console()
             break
     else:
         new_col = Collection(col)
         new_col.items.append(item)
         collections.append(new_col)
+        clear_console()
     
-    
+def create_collection(collections):
+    name = input("Collection name: ")
+    new_collection = Collection(name)
+    for c in collections:
+        if c.name.lower() == name.lower():
+            print("Collection already exists!")
+            time.sleep(3)
+            clear_console()
+            break
+    else:
+        collections.append(new_collection)
+        clear_console()
